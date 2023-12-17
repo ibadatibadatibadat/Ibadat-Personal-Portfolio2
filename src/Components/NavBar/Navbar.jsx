86b269c3acc94c989531f0a7aa6dfcd54fba2c74
ibadatibadatibadat <ibadatafridi7@gmail.com>
2023-12-17 14:19:24 +0500

    Creating And Styling Navbar

---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+import contactImg from "../../assets/contact.png";
+import { Link } from "react-scroll";
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+      <img src={logo} alt="navbar-logo" className="logo" />
+      <div className="DesktopMenu">
+        <Link className="DesktopMenuListItem">Home</Link>
+        <Link className="DesktopMenuListItem">Clients</Link>
+        <Link className="DesktopMenuListItem">Portfolio</Link>
+        <Link className="DesktopMenuListItem">About</Link>
+      </div>
+      <button className="DesktopMenuButton">
+        <img
+          src={contactImg}
+          alt="contact us image"
+          className="DesktopMenuImg"
+        />
+        Contact Me
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
